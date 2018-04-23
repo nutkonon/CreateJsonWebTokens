1b92f705b16875b532e280951fed7e6322c2d10a
Jennarong Srisuwan <nut_konon@hotmail.com>
2018-04-23 12:49:19 +0700

    Add project files.

---
+++ WebApplication1/JsonWebToken.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens;
+
+namespace WebApplication1
+{
+    public enum JwtHashAlgorithm
+    {
+        RS256,
+        HS384,
+        HS512
+    }
+
+    public class JsonWebToken
+    {
+        private static Dictionary<JwtHashAlgorithm, Func<byte[], byte[], byte[]>> HashAlgorithms;
+
+        static JsonWebToken()
+        {
+            HashAlgorithms = new Dictionary<JwtHashAlgorithm, Func<byte[], byte[], byte[]>>
+            {
+                { JwtHashAlgorithm.RS256, (key, value) => { using (var sha = new HMACSHA256(key)) { return sha.ComputeHash(value); } } },
+                { JwtHashAlgorithm.HS384, (key, value) => { using (var sha = new HMACSHA384(key)) { return sha.ComputeHash(value); } } },
+                { JwtHashAlgorithm.HS512, (key, value) => { using (var sha = new HMACSHA512(key)) { return sha.ComputeHash(value); } } }
+            };
+        }
+
+        public string Encode(object payload, string key, JwtHashAlgorithm algorithm)
+        {
+            return Encode(payload, Encoding.UTF8.GetBytes(key), algorithm);
+        }
+
+        public static string Encode(object payload, byte[] keyBytes, JwtHashAlgorithm algorithm)
+        {
+            var segments = new List<string>();
+            var header = new { alg = algorithm.ToString(), typ = "JWT" };
+
+            byte[] headerBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(header, Formatting.None));
+            byte[] payloadBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(payload, Formatting.None));
+
+            segments.Add(Base64UrlEncode(headerBytes));
+            segments.Add(Base64UrlEncode(payloadBytes));
+
+            var stringToSign = string.Join(".", segments.ToArray());
+
+            var bytesToSign = Encoding.UTF8.GetBytes(stringToSign);
+
+            byte[] signature = HashAlgorithms[algorithm](keyBytes, bytesToSign);
+            segments.Add(Base64UrlEncode(signature));
+
+            return string.Join(".", segments.ToArray());
+        }
+
+        private static string Base64UrlEncode(byte[] input)
+        {
+            var output = Convert.ToBase64String(input);
+            output = output.Split('=')[0]; // Remove any trailing '='s
+            output = output.Replace('+', '-'); // 62nd char of encoding
+            output = output.Replace('/', '_'); // 63rd char of encoding
+            return output;
+        }
+
+        private static JwtHashAlgorithm GetHashAlgorithm(string algorithm)
+        {
+            switch (algorithm)
+            {
+                case "RS256": return JwtHashAlgorithm.RS256;
+                case "HS384": return JwtHashAlgorithm.HS384;
+                case "HS512": return JwtHashAlgorithm.HS512;
+                default: throw new InvalidOperationException("Algorithm not supported.");
+            }
+        }
+    }
+}
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Crypto.Parameters;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string jwt = string.Empty;
+            var fileStream = new FileStream(@"Private Key Path", FileMode.Open, FileAccess.Read);
+            AsymmetricCipherKeyPair keypair;
+            using (StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                PemReader pr = new PemReader(streamReader);
+                keypair = (AsymmetricCipherKeyPair)pr.ReadObject();
+            }
+            RSAParameters rsaParams = DotNetUtilities.ToRSAParameters((RsaPrivateCrtKeyParameters)keypair.Private);
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(rsaParams);
+                var payload = new { value = "value" };
+                jwt = Jose.JWT.Encode(payload, rsa, Jose.JwsAlgorithm.RS256);
+            }
+            Label1.Text = jwt;
+        }
+    }
+}
